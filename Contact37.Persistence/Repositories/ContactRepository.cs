126409ae1bddc01b474c8495b85cc430576a6f1d
Gabriel Ricardo <gabriel.rodrigues.ricardo@outlook.com>
2024-10-12 12:11:55 -0300

    refactor: repository layer

---
+++ Contact37.Persistence/Repositories/ContactRepository.cs
@@ -0,0 +1,15 @@
+﻿using Contacts37.Application.Contracts.Persistence;
+using Contacts37.Domain.Entities;
+
+namespace Contact37.Persistence.Repositories
+{
+    public class ContactRepository : GenericRepository<Contact>, IContactRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public ContactRepository(ApplicationDbContext dbContext) : base(dbContext)
+        {
+            _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+        }
+    }
+}
