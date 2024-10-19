7d321788e82a7d0da7bc2ab3e73dcd1de5cedf21
Gabriel Ricardo <gabriel.rodrigues.ricardo@outlook.com>
2024-10-19 17:51:20 -0300

    added validation behavior pipeline

---
+++ Contact37.Persistence/DependencyInjection/PersistenceServicesRegistration.cs
@@ -0,0 +1,28 @@
+﻿using Contacts37.Persistence.Repositories;
+using Contacts37.Application.Contracts.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Contacts37.Persistence.DependencyInjection
+{
+    public static class PersistenceServicesRegistration
+    {
+        public static IServiceCollection ConfigurePersistenceServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(
+                configuration.GetConnectionString("Contact37ConnectionString")));
+
+            services.AddRepositories();
+
+            return services;
+        }
+
+        private static void AddRepositories(this IServiceCollection services)
+        {
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            services.AddScoped<IContactRepository, ContactRepository>();
+        }
+    }
+}
