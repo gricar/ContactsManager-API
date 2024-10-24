eb0beab3f1a397839e68be23c76d801c59bc1728
Rafaela Pessoa <rafaelapessoa.ti@gmail.com>
2024-10-24 11:41:26 -0300

    Get finalizado.

--- Contacts37.Application/Usecases/Contacts/Queries/GetAll/GetAllContactsResponse.cs
+++ Contacts37.Application/Usecases/Contacts/Queries/GetAll/GetAllContactsResponse.cs
@@ -1,4 +1,4 @@
 ﻿namespace Contacts37.Application.Usecases.Contacts.Queries.GetAll
 {
-    public sealed record GetAllContactsResponse(string Name, int DDDCode);
+    public sealed record GetAllContactsResponse(string Name, int DDDCode, string Phone, string Email);
 }
