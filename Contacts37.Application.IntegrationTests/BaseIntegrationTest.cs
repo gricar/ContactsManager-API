0f9f080499c629526dca81bc136a0c1868687bfc
Gabriel Ricardo <gabriel.rodrigues.ricardo@outlook.com>
2024-12-20 10:40:14 -0300

    integration test setup

---
+++ Contacts37.Application.IntegrationTests/BaseIntegrationTest.cs
@@ -0,0 +1,25 @@
+﻿using Contacts37.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Contacts37.Application.IntegrationTests
+{
+    public abstract class BaseIntegrationTest : IClassFixture<IntegrationTestWebAppFactory>
+    {
+        private readonly IServiceScope _scope;
+        protected readonly ISender Sender;
+        protected readonly ApplicationDbContext DbContext;
+
+        protected BaseIntegrationTest(IntegrationTestWebAppFactory factory)
+        {
+            _scope = factory.Services.CreateScope();
+
+            Sender = _scope.ServiceProvider.GetRequiredService<ISender>();
+
+            DbContext = _scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+            DbContext.Database.Migrate();
+        }
+    }
+}
---
+++ Contacts37.Application.IntegrationTests/IntegrationTestWebAppFactory.cs
@@ -0,0 +1,55 @@
+﻿using Contacts37.Persistence;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Testcontainers.MsSql;
+
+namespace Contacts37.Application.IntegrationTests
+{
+    public class IntegrationTestWebAppFactory : WebApplicationFactory<Program>, IAsyncLifetime
+    {
+        private readonly MsSqlContainer _dbContainer;
+
+        public IntegrationTestWebAppFactory()
+        {
+            _dbContainer = new MsSqlBuilder()
+                .WithImage("mcr.microsoft.com/mssql/server:2022-latest")
+                .WithPassword("StrongP@ssword123!")
+                .Build();
+        }
+
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureTestServices(services =>
+            {
+                // Remove o DbContext configurado pela aplicação
+                var descriptor = services
+                    .SingleOrDefault(s => s.ServiceType == typeof(DbContextOptions<ApplicationDbContext>));
+
+                if (descriptor is not null)
+                {
+                    services.Remove(descriptor);
+                }
+
+                // Registra o DbContext usando a connection string do Testcontainers
+                services.AddDbContext<ApplicationDbContext>(options =>
+                {
+                    options.UseSqlServer(_dbContainer.GetConnectionString());
+                });
+            });
+        }
+
+        public Task InitializeAsync()
+        {
+            return _dbContainer.StartAsync();
+        }
+
+
+        public new Task DisposeAsync()
+        {
+            return _dbContainer.StopAsync();
+        }
+    }
+}
