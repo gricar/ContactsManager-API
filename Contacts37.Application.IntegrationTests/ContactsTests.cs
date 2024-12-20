cba752e340c367cf4379255d2cb38712484e1e8f
Gabriel Ricardo <gabriel.rodrigues.ricardo@outlook.com>
2024-12-20 10:40:26 -0300

    contacts test

---
+++ Contacts37.Application.IntegrationTests/ContactsTests.cs
@@ -0,0 +1,108 @@
+﻿using Contacts37.Application.Common.Exceptions;
+using Contacts37.Application.Usecases.Contacts.Commands.Create;
+using Contacts37.Application.Usecases.Contacts.Queries.GetAll;
+using Contacts37.Application.Usecases.Contacts.Queries.GetByDdd;
+using Contacts37.Domain.Entities;
+using FluentAssertions;
+
+namespace Contacts37.Application.IntegrationTests
+{
+    public class ContactsTests : BaseIntegrationTest
+    {
+        public ContactsTests(IntegrationTestWebAppFactory factory) : base(factory)
+        {
+        }
+
+
+        [Fact(DisplayName = "Should create new contact with valid values")]
+        [Trait("Category", "Integration")]
+        public async Task Create_ShouldAddContact_WhenCommandIsValid()
+        {
+            //Arrange
+            var command = new CreateContactCommand("Ayrton", 11, "987654321", "ayrton.senna@example.com");
+
+            //Act
+            var result = await Sender.Send(command);
+
+            //Assert
+            var contact = DbContext.Contacts.FirstOrDefault(c => c.Id == result.Id);
+
+            contact.Should().NotBeNull()
+                .And.BeOfType<Contact>();
+            contact!.Name.Should().NotBeNullOrWhiteSpace()
+                .And.Be("Ayrton");
+            contact.Region.DddCode.Should().BeGreaterThan(0)
+                .And.Be(11);
+            contact.Phone.Should().HaveLength(9)
+                .And.Be("987654321");
+            contact.Email.Should().NotBeNullOrWhiteSpace()
+                .And.Be("ayrton.senna@example.com");
+        }
+
+        [Fact(DisplayName = "Should throw exception when creating new contact with invalid values")]
+        [Trait("Category", "Integration")]
+        public async Task Create_ShouldThrowException_WhenPhoneIsInvalid()
+        {
+            //Arrange
+            var command = new CreateContactCommand("Ayrton", 11, "123", "ayrton.senna@example.com");
+
+            //Act
+            Func<Task> act = async () => await Sender.Send(command);
+
+            //Assert
+            await act.Should().ThrowAsync<BadRequestException>();
+        }
+
+        [Fact(DisplayName = "Should return existing contacts from database")]
+        [Trait("Category", "Integration")]
+        public async Task GetAll_ShouldReturnContactsList_FromDatabase()
+        {
+            //Arrange
+            var command = new CreateContactCommand("Ayrton", 14, "987654321", "ayrton.senna3@example.com");
+            await Sender.Send(command);
+
+            //Act
+            var result = await Sender.Send(new GetAllContactsRequest());
+
+            //Assert
+            result.Should().NotBeNull()
+                .And.HaveCount(1)
+                .And.AllBeOfType<GetAllContactsResponse>()
+                .And.Contain(contact => contact.Name == "Ayrton" && contact.DDDCode == 13);
+        }
+
+        [Fact(DisplayName = "Should return one contact that exists on database")]
+        [Trait("Category", "Integration")]
+        public async Task GetByDdd_ShouldReturnContact_WhenContactExists()
+        {
+            //Arrange
+            var command = new CreateContactCommand("Ayrton", 13, "987654321", "ayrton.senna2@example.com");
+            await Sender.Send(command);
+            var query = new GetContactsByDddRequest(13);
+
+            //Act
+            var result = await Sender.Send(query);
+
+            //Assert
+            result.Should().NotBeNull()
+                .And.HaveCount(3)
+                .And.AllBeOfType<GetContactsByDddResponse>()
+                .And.Contain(contact => contact.Name == "Ayrton" && contact.DDDCode == 13);
+        }
+
+        [Fact(DisplayName = "Should return empty list of contact from the database")]
+        [Trait("Category", "Integration")]
+        public async Task GetByDdd_ShouldReturnEmpty_WhenNoContactsForDddCode()
+        {
+            // Arrange
+            var request = new GetContactsByDddRequest(99);
+
+            // Act
+            var result = await Sender.Send(request);
+
+            // Assert
+            result.Should().NotBeNull()
+                  .And.BeEmpty();
+        }
+    }
+}
