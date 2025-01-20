4dfad53432ad74dae0babc9f053a680472824d22
Gabriel Ricardo <gabriel.rodrigues.ricardo@outlook.com>
2025-01-20 11:17:13 -0300

    refactor: using fixture class

--- Contacts37.Application.Tests/Usecases/Contacts/Queries/GetAllContactsRequestHandlerTests.cs
+++ Contacts37.Application.Tests/Usecases/Contacts/Queries/GetAllContactsRequestHandlerTests.cs
@@ -1,58 +1,42 @@
-﻿using AutoMapper;
-using Bogus;
-using Contacts37.Application.Contracts.Persistence;
+﻿using Contacts37.Application.Contracts.Persistence;
+using Contacts37.Application.Tests.Fixtures;
 using Contacts37.Application.Usecases.Contacts.Queries.GetAll;
 using Contacts37.Domain.Entities;
 using FluentAssertions;
 using Moq;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Contacts37.Application.Tests.Usecases.Contacts.Queries
 {
+    [Collection(nameof(ContactFixtureCollection))]
     public class GetAllContactsRequestHandlerTests
     {
         private readonly Mock<IContactRepository> _contactRepositoryMock;
-        private readonly Faker _faker;
-        private readonly IMapper _mapper;
+        private readonly GetAllContactsRequestHandler _handler;
+        private readonly ContactFixture _fixture;
 
-        public GetAllContactsRequestHandlerTests()
+        public GetAllContactsRequestHandlerTests(ContactFixture fixture)
         {
             _contactRepositoryMock = new Mock<IContactRepository>();
-            _faker = new Faker();
-            var config = new MapperConfiguration(cfg => cfg.AddProfile<GetAllContactsMapper>());
-            _mapper = config.CreateMapper();
+            _handler = new GetAllContactsRequestHandler(_contactRepositoryMock.Object, fixture.Mapper);
+            _fixture = fixture;
         }
 
-        [Fact(DisplayName = "Validate get all contacts")]
+        [Fact(DisplayName = "Should list all contacts successfully when data exists")]
         [Trait("Category", "Get All Contacts - Success")]
         public async void GetAllContacts_ShouldSucceed_WhenContactsExist()
         {
             //Arrange
-            string name = _faker.Person.FirstName;
-            int dddCode = _faker.PickRandom(new[] { 11, 21, 31, 41 });
-            string phone = _faker.Phone.PhoneNumber("#########");
-            string email = _faker.Internet.Email();
-
-            var contacts = new List<Contact>
-            {
-                Contact.Create(name, dddCode, phone, email)
-            };
+            var contacts = _fixture.CreateValidContactList();
 
             _contactRepositoryMock.Setup(repo => repo.GetAllAsync())
                 .ReturnsAsync(contacts);
 
-            var handler = new GetAllContactsRequestHandler(_contactRepositoryMock.Object, _mapper);
-
             // Act
-            var result =  await handler.Handle(new GetAllContactsRequest(), CancellationToken.None);
+            var result =  await _handler.Handle(new GetAllContactsRequest(), CancellationToken.None);
 
             //Assert
             result.Should().BeOfType<List<GetAllContactsResponse>>();
-            result.Should().HaveCount(1);
+            result.Should().HaveCount(5);
         }
 
         [Fact(DisplayName = "Validate get all contacts with empty list")]
@@ -63,10 +47,8 @@ namespace Contacts37.Application.Tests.Usecases.Contacts.Queries
             _contactRepositoryMock.Setup(repo => repo.GetAllAsync())
                 .ReturnsAsync(new List<Contact>());
 
-            var handler = new GetAllContactsRequestHandler(_contactRepositoryMock.Object, _mapper);
-
             // Act
-            var result = await handler.Handle(new GetAllContactsRequest(), CancellationToken.None);
+            var result = await _handler.Handle(new GetAllContactsRequest(), CancellationToken.None);
 
             //Assert
             result.Should().BeOfType<List<GetAllContactsResponse>>();
